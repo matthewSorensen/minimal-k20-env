f2946dcf347fde40c1e3c83f7eea9f271aea5bfd
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-01-14 10:46:36 -0800

    Outline for porting documentation, and example of blinking teensy 3.0 LED in more native manner

---
+++ porting.md
@@ -0,0 +1,17 @@
+#Porting grbl to Freescale's K20
+
+## Toolchain
+
+## Processor Differences
+
+###Timers
+
+###IO
+
+###EEPROM
+
+###USB Serial Communication
+
+## Hardware Configuration
+
+## Per-File Notes
---
+++ timer_test.c
@@ -0,0 +1,37 @@
+#include <mk20dx128.h>
+                  
+#define TIE 2
+#define TEN 1
+
+void timer_setup() {
+  SIM_SCGC6 |= SIM_SCGC6_PIT;
+  PIT_MCR = 0x00;
+  PIT_LDVAL1 = 48000000;
+  PIT_TCTRL1 = TIE;
+  PIT_TCTRL1 |= TEN;  
+  NVIC_ENABLE_IRQ(IRQ_PIT_CH1);
+  PIT_TCTRL2 = TIE;
+  NVIC_ENABLE_IRQ(IRQ_PIT_CH2);
+}
+
+void setup(){
+  PORTC_PCR5 = PORT_PCR_SRE | PORT_PCR_DSE | PORT_PCR_MUX(1);
+  GPIOC_PDDR |= 1<<5;
+  timer_setup();
+}
+
+void pit1_isr(void) {
+  PIT_TFLG1 = 1;
+  GPIOC_PSOR = 1<<5;
+  PIT_LDVAL2 = 480000;
+  PIT_TCTRL2 |= TEN;  
+}
+
+
+void pit2_isr(void) {
+  PIT_TFLG2 = 1;
+  PIT_TCTRL2 = PIT_TCTRL2 & ~(TEN);
+  GPIOC_PCOR = 1<<5;
+}
+
+void loop(void){;}
