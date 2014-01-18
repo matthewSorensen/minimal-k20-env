e317d79caf0be623e13baad545665029eff1cb9b
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-01-17 22:23:36 -0800

    Nice mask

--- vendor/pin_config.h
+++ vendor/pin_config.h
@@ -6,13 +6,14 @@
 #define ISF (1<<24)
 
 // Various interrupt triggering modes
+
+#define IRQC_MASK         0xF0000
 #define IRQC_NONE         0x00000
 #define IRQC_ZERO         0x80000
 #define IRQC_RISING       0x90000
 #define IRQC_FALLING      0xA0000
 #define IRQC_EITHER_EDGE  0xB0000
 #define IRQC_ONE          0xC0000
-
 
 #define PULL_DOWN  2
 #define PULL_UP    3
