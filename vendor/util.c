e71a680880d7742085faea58bf6e5ca2e8903ad5
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-01-23 20:59:04 -0800

    Much more accurate delay implementation

--- vendor/util.c
+++ vendor/util.c
@@ -1,17 +1,12 @@
 #include "util.h"
-void delay(uint32_t ms)
-{
-	uint32_t start = micros();
+extern volatile uint32_t systick_millis_count;
 
-	if (ms > 0) {
-		while (1) {
-			if ((micros() - start) >= 1000) {
-				ms--;
-				if (ms == 0) return;
-				start += 1000;
-			}
-			yield();
-		}
-	}
+void delay(uint32_t ms){
+  uint32_t start = systick_millis_count;
+  uint32_t end = start + ms;
+
+  if(end < start) // If our time interval causes an overflow, wait for the count to overflow and proceed as normal
+    while(systick_millis_count != 0);
+  while(systick_millis_count < end);
 }
 
